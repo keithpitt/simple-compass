ab557a1c494e5b53c577cbadbf9ea02da70cd7dd
Keith Pitt <me@keithpitt.com>
2013-03-01 09:06:27 +1100

    It's kinda working now, the $legacy-x variables are borked though.

--- test.rb
+++ test.rb
@@ -20,21 +20,6 @@ module SimpleCompass
   end
 end
 
-module Sass::Script::Functions
-  def legacy_support_for_ie6
-    false
-  end
-  def legacy_support_for_ie7
-    false
-  end
-  def legacy_support_for_ie8
-    false
-  end
-end
-
 options = { :load_paths => [ Sass::Importers::Filesystem.new("sass") ] }
 engine = ::Sass::Engine.new File.read("test/fixtures/opacity_test.sass"), options
-p engine.environment
-
-
 p engine.to_css
