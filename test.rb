19d1df4627b020e4fd88671f5b9a3136ada4829a
Keith Pitt <me@keithpitt.com>
2013-03-01 08:06:17 +1100

    Oooh, basic importing works.

--- test.rb
+++ test.rb
@@ -2,11 +2,24 @@ require 'rubygems'
 require "bundler/setup"
 require "sass"
 
-class SimpleCompass < Sass::Importers::Base
-  def initialize(root)
-    raise 'root'
+module SimpleCompass
+  class Importer < Sass::Importers::Base
+    def find(name, context = nil)
+      if name =~ /simple_compass\//
+        Sass::Engine.new("", :filename => name, :importer => self)
+      end
+    end
+
+    def key(name, context)
+      [self.class.name, name]
+    end
+
+    def to_s
+      "simple_compass"
+    end
   end
 end
 
-engine = ::Sass::Engine.new File.read("stylesheets/test.sass")
+options = { :load_paths => [ SimpleCompass::Importer.new ] }
+engine = ::Sass::Engine.new File.read("stylesheets/test.sass"), options
 p engine.to_css
