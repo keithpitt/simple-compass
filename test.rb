aef9d9eb8d1295f9928fde2341502d1aab8a0b48
Keith Pitt <me@keithpitt.com>
2013-03-01 07:56:47 +1100

    Yay, I've got some CSS rendering.

--- test.rb
+++ test.rb
@@ -2,4 +2,11 @@ require 'rubygems'
 require "bundler/setup"
 require "sass"
 
-p Sass
+class SimpleCompass < Sass::Importers::Base
+  def initialize(root)
+    raise 'root'
+  end
+end
+
+engine = ::Sass::Engine.new File.read("stylesheets/test.sass")
+p engine.to_css
