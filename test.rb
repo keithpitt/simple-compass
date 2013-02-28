f6a6ea757c96b2f45f394f11a48fdd9f2e51b72d
Keith Pitt <me@keithpitt.com>
2013-03-01 08:58:25 +1100

    Trying to get the opacity function to work.

--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ module SimpleCompass
   class Importer < Sass::Importers::Base
     def find(name, context = nil)
       if name =~ /simple_compass\//
-        Sass::Engine.new("", :filename => name, :importer => self)
+        Sass::Engine.new(File.read("sass/simple_compass.sass"), :filename => name, :importer => self)
       end
     end
 
@@ -20,6 +20,21 @@ module SimpleCompass
   end
 end
 
-options = { :load_paths => [ SimpleCompass::Importer.new ] }
+module Sass::Script::Functions
+  def legacy_support_for_ie6
+    false
+  end
+  def legacy_support_for_ie7
+    false
+  end
+  def legacy_support_for_ie8
+    false
+  end
+end
+
+options = { :load_paths => [ Sass::Importers::Filesystem.new("sass") ] }
 engine = ::Sass::Engine.new File.read("test/fixtures/opacity_test.sass"), options
+p engine.environment
+
+
 p engine.to_css
